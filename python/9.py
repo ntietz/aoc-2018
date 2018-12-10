f77793eb7b14e87667ffc2cdc8ed11ba22a6b34f
Nicholas Tietz-Sokolsky <git@ntietz.com>
2018-12-09 20:41:20 -0500

    Add deque-based solution

--- python/9.py
+++ python/9.py
@@ -1,6 +1,16 @@
+from collections import deque
+from typing import Dict
+
+
+try:
+    profile
+except NameError:
+    def profile(f):
+        return f
+
 
 @profile
-def play(num_players: int, last_marble: int) -> None:
+def list_play(num_players: int, last_marble: int) -> None:
     marbles = [0]
     scores: Dict[int, int] = {}
     for player in range(0, num_players):
@@ -21,13 +31,36 @@ def play(num_players: int, last_marble: int) -> None:
     print(max(scores.values()))
 
 
+@profile
+def play(num_players: int, last_marble: int) -> None:
+    marbles = deque([0])
+    scores: Dict[int, int] = {}
+    for player in range(0, num_players):
+        scores[player] = 0
+    for marble in range(1, last_marble+1):
+        if marble % 23 == 0:
+            marbles.rotate(-7)
+            taken = marbles.pop()
+            scores[marble % num_players] += marble + taken
+        else:
+            marbles.rotate(2)
+            marbles.append(marble)
+    print(max(scores.values()))
+
+
 if __name__ == '__main__':
-    play(9, 25)
-    play(10, 161)
-    play(13, 7999)
-    play(17, 1104)
-    play(21, 6111)
-    play(30, 5807)
-    #play(403, 71920)
-    #play(403, 7192000)
+    #play(9, 25)
+    #list_play(9, 25)
+    #play(10, 161)
+    #list_play(10, 161)
+    #play(13, 7999)
+    #list_play(13, 7999)
+    #play(17, 1104)
+    #list_play(17, 1104)
+    #play(21, 6111)
+    #list_play(21, 6111)
+    #play(30, 5807)
+    #list_play(30, 5807)
+    play(403, 71920)
+    play(403, 7192000)
 
